b984ef9962b02ef7154c86397ef279a6d564cbe6
jikho7 <113023191+jikho7@users.noreply.github.com>
2023-02-07 18:41:12 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: jdefayes <jdefayes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/01 00:21:04 by jdefayes          #+#    #+#             */
-/*   Updated: 2023/02/07 18:40:40 by jdefayes         ###   ########.fr       */
+/*   Updated: 2023/02/07 18:41:03 by jdefayes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,13 +26,13 @@ int	main(int ac, char**av)
 	{
 		data.av1 = &av[1];
 		data.z_julia = 1;
-		data.fra = (t_fra){0, 0, 0, 0, 0, 0, 0, 0xffffff, 1, 1, 1, 0, -2, 0};		// struct init
+		data.fra = (t_fra){0, 0, 0, 0, 0, 0, 0, 0xffffff, 1, 1, 1, 0, -2, 0};	// struct init
 
-		init_mlx(&data);					// create window, image and get image datas
+		init_mlx(&data);							// create window, image and get image datas
 
-		mlx_mouse_hook(data.mlx_win, &mouse_controls, &data);				// HOOKS PART
-		mlx_hook(data.mlx_win, 17, 0, &close_window, &data);				// to close window properly
-		mlx_hook(data.mlx_win, 2, 0, &keyboard_input, &data);				// to move smoothly on the window :)
+		mlx_mouse_hook(data.mlx_win, &mouse_controls, &data);			// HOOKS PART
+		mlx_hook(data.mlx_win, 17, 0, &close_window, &data);			// to close window properly
+		mlx_hook(data.mlx_win, 2, 0, &keyboard_input, &data);			// to move smoothly on the window :)
 		mlx_key_hook(data.mlx_win, &keyboard_input, &data);
 		mlx_loop_hook(data.mlx, &render, &data);
 		mlx_loop(data.mlx);
