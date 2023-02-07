77957c04177b26a89fe85e7403efb3a21b44363b
jikho7 <113023191+jikho7@users.noreply.github.com>
2023-02-07 18:37:08 +0100

    Add files via upload

--- hooks.c
+++ hooks.c
@@ -6,7 +6,7 @@
 /*   By: jdefayes <jdefayes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/01 00:37:44 by jdefayes          #+#    #+#             */
-/*   Updated: 2023/02/07 18:15:43 by jdefayes         ###   ########.fr       */
+/*   Updated: 2023/02/07 18:36:45 by jdefayes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,10 @@
 
 int	keyboard_input(int keycode, t_data *data)
 {
-	if (keycode == 53)							// esc
+	if (keycode == 53)						// esc
 		reject();
 
-	if (keycode == 124 && data->fra.z < 50)		// arrows pad
+	if (keycode == 124 && data->fra.z < 50)			// arrows pad
 		data->fra.l_r += 10 * data->fra.z;
 	if (keycode == 124 && data->fra.z > 50)
 		data->fra.l_r += 100;
@@ -35,7 +35,7 @@ int	keyboard_input(int keycode, t_data *data)
 	if (keycode == 126 && data->fra.z > 50)
 		data->fra.u_d -= 100;
 
-	if (keycode == 78)							// julia constant incre-decrementation
+	if (keycode == 78)					// julia constant incre-decrementation
 		data->fra.multi += 0.032;
 	if (keycode == 69)
 		data->fra.multi -= 0.032;
@@ -46,19 +46,19 @@ int	mouse_controls(int mousecode, int x, int y, t_data *data)
 {
 	(void)x;
 	(void)y;
-	if (mousecode == 4 && data->fra.z <= 40)	//	mandel/burn zoom in
+	if (mousecode == 4 && data->fra.z <= 40)		//mandel/burn zoom in
 		data->fra.z *= 1.2;
 	if (mousecode == 4 && data->fra.z > 40)
 		data->fra.z += 6;
-	if (mousecode == 5)							// m/b zoom out
+	if (mousecode == 5)					// m/b zoom out
 		data->fra.z *= 0.8;
 
-	if (mousecode == 4)							// julia zoom
+	if (mousecode == 4)					// julia zoom
 		data->z_julia *= 1.2;
 	if (mousecode == 5)
 		data->z_julia *= 0.8;
 
-	if (mousecode == 2)							// reset, back to inital image
+	if (mousecode == 2)					// reset, back to inital image
 	{
 		data->fra.u_d = 1;
 		data->fra.l_r = 1;
@@ -66,7 +66,7 @@ int	mouse_controls(int mousecode, int x, int y, t_data *data)
 		data->z_julia = 1;
 	}
 
-	if (mousecode == 1)							// to change colors
+	if (mousecode == 1)					// to change colors
 		data->fra.c = data->fra.c + rand();
 	return (0);
 }
