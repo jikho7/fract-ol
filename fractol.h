a10ca980e048da57b1a2265bb86ee7f2f257b4d4
jikho7 <113023191+jikho7@users.noreply.github.com>
2023-02-07 18:32:53 +0100

    Add files via upload

--- fractol.h
+++ fractol.h
@@ -6,7 +6,7 @@
 /*   By: jdefayes <jdefayes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/17 13:41:09 by jdefayes          #+#    #+#             */
-/*   Updated: 2023/02/07 17:45:48 by jdefayes         ###   ########.fr       */
+/*   Updated: 2023/02/07 18:32:21 by jdefayes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,11 +32,11 @@ typedef struct s_fra	// structre for all fractales
 	double	c_r;	//real axe coordinates
 	double	c_i;	//imaginary axe coordinates
 	double	new;
-	int		c;		// color
-	double	z;		// zoom factor
+	int	c;	// color
+	double	z;	// zoom factor
 	double	l_r;	// left_right
 	double	u_d;	// up_down
-	int		i;		// iteration
+	int	i;	// iteration
 	double	multi;	// julia constante
 	char	*zoom;	// zoom display
 }	t_fra;
@@ -45,9 +45,9 @@ typedef struct s_img
 {
 	void	*img_ptr;
 	char	*addr;
-	int		bpp;
-	int		line_len;
-	int		endian;
+	int	bpp;
+	int	line_len;
+	int	endian;
 }	t_img;
 
 typedef struct s_data
@@ -56,31 +56,31 @@ typedef struct s_data
 	void	*mlx_win;
 	t_img	img;
 	t_fra	fra;
-	int		x;
-	int		y;
+	int	x;
+	int	y;
 	char	**av1;
-	int		len;
-	int		check;
+	int	len;
+	int	check;
 	double	z_julia;	// julia zoom
 }	t_data;
 
-int		main(int ac, char**av);
-int		init_mlx(t_data *data);
-int		close_window(t_data *data);
+int	main(int ac, char**av);
+int	init_mlx(t_data *data);
+int	close_window(t_data *data);
 
-int		mouse_controls(int mousecode, int x, int y, t_data *data);
-int		keyboard_input(int keycode, t_data *data);
+int	mouse_controls(int mousecode, int x, int y, t_data *data);
+int	keyboard_input(int keycode, t_data *data);
 
-int		fra_strlen(const char *s);
-int		fra_strncmp(const char *s1, const char *s2, size_t n);
-int		render(t_data *data);
+int	fra_strlen(const char *s);
+int	fra_strncmp(const char *s1, const char *s2, size_t n);
+int	render(t_data *data);
 void	pixel_put(t_img *img, int x, int y, int color);
-int		set_color(t_fra *fra, t_img *img);
+int	set_color(t_fra *fra, t_img *img);
 char	*fra_itoa(int n);
 
-int		mandelbrot(t_img *img, t_fra fra, t_data *data);
-int		julia(t_img *img, t_fra fra, t_data *data);
-int		burning_ship(t_img *img, t_fra fra);
+int	mandelbrot(t_img *img, t_fra fra, t_data *data);
+int	julia(t_img *img, t_fra fra, t_data *data);
+int	burning_ship(t_img *img, t_fra fra);
 
 void	reject(void);
 #endif
